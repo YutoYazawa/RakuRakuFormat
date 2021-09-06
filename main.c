b0afb27cf6e9cdba8f88f772cd50a4e8ef5d2780
Yuto Yazawa <008900012151b5yr@gl.pen-kanagawa.ed.jp>
2021-09-06 23:18:35 +0900

    最低限動くようにした

--- main.c
+++ main.c
@@ -9,14 +9,25 @@
 #include <string.h>
 #include "stredit.h"
 #define STR_SIZE 1600
+#define PUB "<p id=\"publ\"></p>"
+#define DEST "<p id=\"dest\"></p>"
+#define TITLE "<p id=\"titl\"></p>"
+#define MAIN "<p id=\"main\"></p>"
 
 int main(void){
 
 	FILE *fp;
 	char str[STR_SIZE+1];
 	char body[STR_SIZE-1];
+	char html[512];
+	char temp[STR_SIZE] = "<html>\n<head>\n<link rel=\"stylesheet\" href=\"style.css\">\n</link>\n</head>\n<body>\n</body>\n</html>\n";
+	char puff[64];
 	char prefix[3];
 	char macro[5];
+	char kmain[STR_SIZE]="・";
+	char atag[STR_SIZE*2];
+	char btag[16];
+	char tempb[STR_SIZE];
 
 	fp = fopen("入力.txt","r");
 	if (fp==NULL) {
@@ -28,25 +39,62 @@ int main(void){
 		getstr(str, 0, 2, prefix, sizeof(prefix));
 		getstr(str, 2, STR_SIZE-2, body, sizeof(body));
 		if (!strcmp(prefix,"##")) {
-			printf("タイトル:%s\n", body);
+			getstr(TITLE, 0, 13, atag, sizeof(atag));
+			getstr(TITLE, 13, 4, btag, sizeof(btag));
+			strcat(atag,body);
+			strcat(atag,btag);
+			strcat(html,atag);
 		}
 		else if (!strcmp(prefix,">>")) {
-			printf("発行人:%s\n", body);
+			getstr(PUB, 0, 13, atag, sizeof(atag));
+            getstr(PUB, 13, 4, btag, sizeof(btag));
+			strcat(atag,body);
+            strcat(atag,btag);
+			strcat(html,atag);
 		}
 		else if (!strcmp(prefix,"<<")) {
-			printf("宛先:%s\n", body);
+			getstr(DEST, 0, 13, atag, sizeof(atag));
+            getstr(DEST, 13, 4, btag, sizeof(btag));
+			strcat(atag,body);
+            strcat(atag,btag);
+			strcat(html,atag);
 		}
 		else if (!strcmp(prefix,"--")) {
-			printf("箇条書き:%s\n", body);
+			getstr(MAIN, 0, 13, atag, sizeof(atag));
+            getstr(MAIN, 13, 4, btag, sizeof(btag));
+			strcat(kmain,body);
+			strcat(atag,body);
+            strcat(atag,btag);
+			strcat(html,atag);
 		}
 		else if (!strcmp(prefix,"$$")) {
+			getstr(MAIN, 0, 13, atag, sizeof(atag));
+            getstr(MAIN, 13, 4, btag, sizeof(btag));
 			getstr(body, 0, 4, macro, sizeof(macro));
-			printf("マクロ名:%s\n",macro);
+			getstr(body, 4, STR_SIZE-6, body, sizeof(body));
+			strcat(atag,str);
+            strcat(atag,btag);
+			strcat(html,atag);
 		}
 		else {
-			printf("本文:%s\n", str);
+			getstr(MAIN, 0, 13, atag, sizeof(atag));
+            getstr(MAIN, 13, 4, btag, sizeof(btag));
+			strcat(atag,str);
+            strcat(atag,btag);
+			strcat(html,atag);
 		}
 	}
+	getstr(temp, 0, 77, tempb, sizeof(tempb));
+	getstr(temp, 77, sizeof(temp)-77, temp, sizeof(temp));
+	strcat(tempb,html);
+	strcat(tempb,temp);
+	printf("\n%s\n",tempb);
+
+	fclose(fp);
+
+	fp = fopen("出力.html", "w");
+
+	fprintf(fp, "%s\n", tempb);
 
 	fclose(fp);
 
